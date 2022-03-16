1e54a7f939684325583c68b7c0ed1622d230ed6a
Smakosh <ismai23l@hotmail.com>
2022-03-16 16:16:43 +0100

    docs: generated docs using nextra theme docs

---
+++ docs/theme.config.js
@@ -0,0 +1,35 @@
+export default {
+  github: "https://github.com/Ontwik-Dev/ontwik-ui",
+  docsRepositoryBase:
+    "https://github.com/Ontwik-Dev/ontwik-ui/blob/master/pages",
+  branch: "master",
+  path: "/",
+  titleSuffix: " – Ontwik UI",
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null,
+  darkMode: false,
+  footer: true,
+  footerText: "MIT 2021 © Smakosh",
+  footerEditOnGitHubLink: true, // will link to the docs repo
+  logo: (
+    <>
+      <span>React-flex-ready -</span>
+      <span> A Flexbox grid ready, easy to implement and customize</span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="description"
+        content="Ontwik UI: the headless UI React library"
+      />
+      <meta
+        name="og:title"
+        content="Ontwik UI: the headless UI React library"
+      />
+    </>
+  )
+};
